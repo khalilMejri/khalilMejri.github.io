a780eafe292b1c0bda86b229aeec550025ceb73e
khalil mejri <khalil.mejri.39566@gmail.com>
2019-10-07 17:14:45 +0200

    add new version

--- app.js
+++ app.js
@@ -6,61 +6,11 @@ var app = new Vue({
         activeCameraId: null,
         cameras: [],
         scans: [],
-        properties: [Object]
+        properties: [Object],
+        code: null
     },
     mounted: function () {
-        var self = this;
-
-        self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false, scanPeriod: 5 });
-        self.scanner.addListener('scan', function (content, image) {
-            self.scans.unshift({ date: +(Date.now()), content: content });
-            // encode the generated code
-            var codeBase64 = window.btoa(content)
-            // make Ajax Call requesting the equivalent code  
-            $.ajax({
-                type: 'POST',
-                url: 'https://prohairlabs.azurewebsites.net/codes/scan/' + codeBase64,
-                // url: '192.168.1.6:5001/codes/scan/' + codeBase64,
-                success: function (data) {
-                    console.log(data)
-                },
-                error: function (err) {
-                    console.log(err.statusText);
-                }
-            })
-            // // do whatever with scanned code
-            // self.properties = [Object];
-            // var productInfos = {
-            //     "Ref": content,
-            //     "Display": "21.5-inch (diagonal) Retina 4K display",
-            //     "Processor": "3.6GHz quad‑core Intel Core i3",
-            //     "Memory": "8GB of 2400MHz DDR4 memory",
-            //     "Storage": "1TB (5400-rpm) hard drive",
-            //     "Grapihcs": "Radeon Pro 555X with 2GB of GDDR5 memory",
-            //     "Video Support and Camera": "FaceTime HD camera <br> Simultaneously supports full native resolution on the built-in display at millions of colors (21.5-inch) or 1 billion colors (21.5-inch 4K)",
-            //     "Audio": "Stereo speakers <br> Microphone <br> 3.5 mm headphone jack",
-            //     "Size and Weight": "21.5-inch iMac <br> Height: 17.7 inches (45.0 cm) <br>  Width: 20.8 inches (52.8 cm) <br> Stand depth: 6.9 inches (17.5 cm) <br> Weight: 12.5 pounds (5.66 kg)2"
-            // }
-            // for (var key in productInfos) {
-            //     if (productInfos.hasOwnProperty(key)) {
-            //         self.properties.push({ mKey: key, value: productInfos[key] });
-            //     }
-            // }
-            // console.log(self.properties);
-
-        });
-        Instascan.Camera.getCameras().then(function (cameras) {
-            self.cameras = cameras;
-            if (cameras.length > 0) {
-                self.activeCameraId = cameras[0].id;
-                self.scanner.start(cameras[0]);
-                console.log(cameras);
-            } else {
-                console.error('No cameras found.');
-            }
-        }).catch(function (e) {
-            console.error(e);
-        });
+        console.log("Vue js application mounted successfully")
     },
     methods: {
         formatName: function (name) {
@@ -76,6 +26,65 @@ var app = new Vue({
             })
             this.activeCameraId = null;
 
+        },
+        startScan: function () {
+            $("html, body").animate({ scrollTop: $(document).height() }, 1000); var self = this;
+            self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false, scanPeriod: 5 });
+            self.scanner.addListener('scan', function (content, image) {
+                self.scans.unshift({ date: +(Date.now()), content: content });
+                // encode the generated code
+                var codeBase64 = window.btoa(content)
+                self.code = content;
+                // make Ajax Call requesting the equivalent code  
+                $.ajax({
+                    type: 'POST',
+                    url: 'https://prohairlabs.azurewebsites.net/codes/scan/' + codeBase64,
+                    // url: '192.168.1.6:5001/codes/scan/' + codeBase64,
+                    success: function (data) {
+                        console.log(data)
+                        $("html, body").animate({ scrollTop: $(document).height() }, 1000); var self = this;
+                        // stop scan
+                        self.stopScan()
+                    },
+                    error: function (err) {
+                        console.log(err.statusText);
+                        self.stopScan()
+                    }
+                })
+                // do whatever with scanned code
+                self.properties = [Object];
+                var productInfos = {
+                    "Ref": codeBase64,
+                    "Display": "21.5-inch (diagonal) Retina 4K display",
+                    "Processor": "3.6GHz quad‑core Intel Core i3",
+                    "Memory": "8GB of 2400MHz DDR4 memory",
+                    "Storage": "1TB (5400-rpm) hard drive",
+                    "Grapihcs": "Radeon Pro 555X with 2GB of GDDR5 memory",
+                    "Video Support and Camera": "FaceTime HD camera <br> Simultaneously supports full native resolution on the built-in display at millions of colors (21.5-inch) or 1 billion colors (21.5-inch 4K)",
+                    "Audio": "Stereo speakers <br> Microphone <br> 3.5 mm headphone jack",
+                    "Size and Weight": "21.5-inch iMac <br> Height: 17.7 inches (45.0 cm) <br>  Width: 20.8 inches (52.8 cm) <br> Stand depth: 6.9 inches (17.5 cm) <br> Weight: 12.5 pounds (5.66 kg)2"
+                }
+                for (var key in productInfos) {
+                    if (productInfos.hasOwnProperty(key)) {
+                        self.properties.push({ mKey: key, value: productInfos[key] });
+                    }
+                }
+                console.log(self.properties);
+
+            });
+            Instascan.Camera.getCameras().then(function (cameras) {
+                self.cameras = cameras;
+                if (cameras.length > 0) {
+                    self.activeCameraId = cameras[0].id;
+                    self.scanner.start(cameras[0]);
+                    console.log(cameras);
+                } else {
+                    console.error('No cameras found.');
+                    $(".preview-container").addClass("error");
+                }
+            }).catch(function (e) {
+                console.error(e);
+            });
         }
     }
 });
